a1134661140bb2c690148590381ec4475a16a81e
Guilherme Henrique Galdini Tosi <87606621+Tosiapenas@users.noreply.github.com>
2022-04-10 21:44:18 -0300

    Update water_consum.c

--- Codebench/water_consum.c
+++ Codebench/water_consum.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(){
 	
-	float consumo_moradores, consumo_final, capacidade, volume, alt, larg, comp, autonomia;
+	float consumo_moradores, capacidade, volume, alt, larg, comp, autonomia;
 	
 	scanf("%f %f %f", &alt, &larg, &comp); //volume
 	scanf("%f", &consumo_moradores); //litros/dia
+
+     if(consumo_moradores < 0){
+        consumo_moradores *= -1;
+    }
+	
+	if(alt < 0){
+        alt *= -1;
+    }
+	
+	if(larg < 0){
+        larg *= -1;
+    }
+	
+	if(comp < 0){
+        comp *= -1;
+    }
 	
 	volume = alt*larg*comp; //cm^3
 	
 	capacidade = volume / 1000;
 
 	autonomia = capacidade / consumo_moradores;
-	
+
+	printf("Valores corretos, agora vou calcular...em\n");
+
+	for(int i = 3; i > 0 ; i--){
+		printf("%d\n", i);
+		sleep(1);
+	}
+
 	if(autonomia < 2.0){
-		printf("Capacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Elevado", capacidade, autonomia);
+		printf("\nCapacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Elevado\n", capacidade, autonomia);
 	} else if(autonomia >= 2.0 && autonomia < 7.0){
-		printf("Capacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Moderado", capacidade, autonomia);
+		printf("\nCapacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Moderado\n", capacidade, autonomia);
 	}else
-		printf("Capacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Reduzido", capacidade, autonomia);
+		printf("\nCapacidade do Reservatorio= %.1f litros\nAutonomia do reservatorio= %.1f dias\nConsumo Reduzido\n", capacidade, autonomia);
 	
 	return 0;
 }
