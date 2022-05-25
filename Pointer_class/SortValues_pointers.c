14f29b8fe7f12d1a0d63eb31f5069a7730ca75d5
Guilherme Henrique Galdini Tosi <galdiniguilherme@usp.br>
2022-05-25 11:52:50 -0300

    Update SortValues_pointers.c

--- Pointer_class/SortValues_pointers.c
+++ Pointer_class/SortValues_pointers.c
@@ -15,11 +15,13 @@ int main(){
 
     if((*p2 >= p3)){
 
-      printf("%d %d %d", *p3, *p2, *p1);
+      printf("%d %d %d\n", *p3, *p2, *p1);
+      printf("%d %d %d", n1, n2, n3);
 
     }else if((*p3 > p2)){
 
-      printf("%d %d %d", *p2, *p3, *p1);
+      printf("%d %d %d\n", *p2, *p3, *p1);
+      printf("%d %d %d", n1, n2, n3);
     }
 
 }
@@ -27,11 +29,13 @@ else if((*p2 >= *p1) && (*p2 >= *p3)){
 
   if(*p1 >= *p3){
 
-     printf("%d %d %d", *p3, *p1, *p2);
+     printf("%d %d %d\n", *p3, *p1, *p2);
+     printf("%d %d %d", n3, n1, n2);
 
   }else if(*p3 >= *p1){
 
-     printf("%d %d %d", *p1, *p3, *p2);
+     printf("%d %d %d\n", *p1, *p3, *p2);
+     printf("%d %d %d", n1, n3, n2);
 
   }
 
@@ -40,12 +44,14 @@ else if((*p3 >= *p1) && (*p3 >= *p2)){
 
   if(*p1 >= *p2){
 
-     printf("%d %d %d", *p2, *p1, *p3);
+     printf("%d %d %d\n", *p2, *p1, *p3);
+     printf("%d %d %d", n2, n1, n3); 
 
   }
   else if(*p2 >= *p1){
 
-     printf("%d %d %d", *p1, *p2, *p3);
+     printf("%d %d %d\n", *p1, *p2, *p3);
+     printf("%d %d %d", n1, n2, n3);
   }
 }
 
