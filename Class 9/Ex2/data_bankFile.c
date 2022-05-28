e55e1067a87360a767c60a6c4e009945ac3c209c
Guilherme Henrique Galdini Tosi <galdiniguilherme@usp.br>
2022-05-28 13:24:34 -0300

    Update data_bankFile.c

--- Class 9/Ex2/data_bankFile.c
+++ Class 9/Ex2/data_bankFile.c
@@ -3,13 +3,16 @@
 
 int main() {
 
-FILE *file = fopen("ex2.txt", "r");
+char file_option[20];
+scanf("%20s", file_option);
+
+FILE *file = fopen(file_option, "r");
 char name[100], job[100];
 int age;
 double wage_min, wage;
 
 if(file == NULL){
-  printf("Erro na leitura do arquivo");
+  printf("Este banco de dados não existe");
   exit(1);
 }
 
@@ -18,7 +21,7 @@ scanf("%lf", &wage_min);
 while (fscanf (file, "%s %d %s %lf", name, &age, job, &wage) != EOF)
   {
    
-    if(wage >= wage_min){
+    if(wage > wage_min){
         printf ("%s\n%d\n%s\n%.0lf\n", name, age, job, wage);
     }
 
